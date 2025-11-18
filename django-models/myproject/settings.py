7d10533b985436b4614519b7555989f7faf5d95b
Amr Khaled <amrkh3bkarino10@gmail.com>
2025-11-18 06:20:07 +0200

    Modify TEMPLATES setting to include app templates
    Updated TEMPLATES DIRS to include relationship_app templates.

--- django-models/myproject/settings.py
+++ django-models/myproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'relationship_app' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
