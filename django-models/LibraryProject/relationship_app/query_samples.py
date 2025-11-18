5f383004d812a1fbc38625b02e49f583f50b636d
Amr Khaled <amrkh3bkarino10@gmail.com>
2025-11-18 06:00:31 +0200

    Refactor query functions for books and librarians

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,25 +1,16 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query all books by a specific author
+# 1. Query all books by a specific author
 def books_by_author(author_name):
-    try:
-        author = Author.objects.get(name=author_name)
-        return author.books.all()
-    except Author.DoesNotExist:
-        return []
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
 
-# List all books in a library
+# 2. List all books in a library
 def books_in_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.books.all()
-    except Library.DoesNotExist:
-        return []
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
 
-# Retrieve the librarian for a library
+# 3. Retrieve the librarian for a library
 def librarian_for_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.librarian
-    except Library.DoesNotExist:
-        return None
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
