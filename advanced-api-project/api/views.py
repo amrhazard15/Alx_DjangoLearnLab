c292d2abd69faa2a7bba5afb117066d6dd4da6b4
Amr Khaled <amrkh3bkarino10@gmail.com>
2025-11-30 02:25:43 +0200

    Import django_filters in views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
+from django_filters import rest_framework
 
 
 # ===================== BOOK VIEWS =====================
