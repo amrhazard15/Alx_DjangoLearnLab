0d830bec7306dbccb2dea81f0bb019a6b4f4ae49
Amr Khaled <amrkh3bkarino10@gmail.com>
2025-12-15 01:31:37 +0200

    Update Django settings with environment variable support

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PORT = os.environ.get("PORT", "8000")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -139,4 +139,5 @@ SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
 SECURE_SSL_REDIRECT = False  
+
 
