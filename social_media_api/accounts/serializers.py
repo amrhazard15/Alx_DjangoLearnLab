ec6f457b02dca90854884352583b28148333b2c9
Amr Khaled <amrkh3bkarino10@gmail.com>
2025-12-14 23:38:28 +0200

    Refactor RegisterSerializer for clarity

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password', 'bio', 'profile_picture')
@@ -23,4 +23,6 @@ class RegisterSerializer(serializers.ModelSerializer):
         
         Token.objects.create(user=user)
         return user
+
+
 
