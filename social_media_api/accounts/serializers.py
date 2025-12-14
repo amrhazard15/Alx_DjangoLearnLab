87834af90be3bf1be9b8980c9b4f5c093c7192db
Amr Khaled <amrkh3bkarino10@gmail.com>
2025-12-14 23:31:51 +0200

    Refactor RegisterSerializer for user creation
    Refactor RegisterSerializer to use get_user_model and create_user method. Added automatic token creation upon user registration.

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,22 +1,25 @@
 from rest_framework import serializers
-from .models import CustomUser
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 from django.contrib.auth.password_validation import validate_password
 
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-
+    
     class Meta:
-        model = CustomUser
+        model = User
         fields = ('username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
-        user = CustomUser.objects.create(
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
+            password=validated_data['password'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
-        user.set_password(validated_data['password'])
-        user.save()
+        # إنشاء توكن تلقائي
+        Token.objects.create(user=user)
         return user
