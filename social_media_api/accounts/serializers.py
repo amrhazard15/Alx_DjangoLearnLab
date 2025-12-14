36e1e579d1c7de6d3c87f83ebcc1c46c5c57b310
Amr Khaled <amrkh3bkarino10@gmail.com>
2025-12-14 23:42:17 +0200

    Modify RegisterSerializer to remove password validation
    Removed password validation from RegisterSerializer.

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -6,7 +6,7 @@ from django.contrib.auth.password_validation import validate_password
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+     password = serializers.CharField()
 
     class Meta:
         model = User
@@ -23,6 +23,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         
         Token.objects.create(user=user)
         return user
+
 
 
 
