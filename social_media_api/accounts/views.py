edbb75ec62c7a181ae83dc35ba4d1edfae173165
Amr Khaled <amrkh3bkarino10@gmail.com>
2025-12-15 00:18:43 +0200

    Refactor Follow/Unfollow views to use GenericAPIView

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -31,13 +31,13 @@ class LoginView(APIView):
         return Response({"error": "Invalid Credentials"}, status=400)
 
 
-class FollowUserView(APIView):
+class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
         try:
-            user_to_follow = User.objects.get(id=user_id)
-        except User.DoesNotExist:
+            user_to_follow = CustomUser.objects.all()
+        except CustomUser.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
         
         if user_to_follow == request.user:
@@ -46,17 +46,19 @@ class FollowUserView(APIView):
         request.user.following.add(user_to_follow)
         return Response({"message": f"You are now following {user_to_follow.username}"})
 
-class UnfollowUserView(APIView):
+
+class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
         try:
-            user_to_unfollow = User.objects.get(id=user_id)
-        except User.DoesNotExist:
+            user_to_unfollow = CustomUser.objects.all()
+        except CustomUser.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
         
         request.user.following.remove(user_to_unfollow)
         return Response({"message": f"You have unfollowed {user_to_unfollow.username}"})
+
 
 
 
