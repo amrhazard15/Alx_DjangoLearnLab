01cd0d83a848bcae33cb97ebb2643fcd4665227a
Amr Khaled <amrkh3bkarino10@gmail.com>
2025-12-15 00:57:32 +0200

    Add notifications URL pattern to urlpatterns

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('api/', include('posts.urls')),
+    path('api/', include('notifications.urls')),
 ]
+
 
